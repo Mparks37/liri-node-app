964952f71b62f5ac9030a0b055a2d4e873b93dba
Mike <mparks37@gmail.com>
2017-11-03 20:47:53 -0400

    adding updates and NPM files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<gsjCu4pRGeGiz9ldnG7RneKRq>',
+  consumer_secret: '<HUjFq7XBih0p2BNIpQ96J8wGEgllaN4heWMj6hM9Rp7b5PEMQb>',
+  access_token_key: '<925200470876057600-hNVuRLGc84oI77nFSGypiI9iFEyOq96>',
+  access_token_secret: '<BxRJbZ6gQ1V8StYQCUKEJBTHalHPcVE7bdgyEodiTBqU6>',
+}
+
+module.exports = twitterKeys;
